5d9984a1465c9993dfe7127f8da818bb362421d9
Hero-Gamer727 <83517823+Hero-Gamer727@users.noreply.github.com>
2022-02-04 16:24:07 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ rightwristX=0;
 rightwristY=0;
 leftwristX=0;
 leftwristY=0;
-
+score=0;
+song_status="";
 function preload(){
-    song = loadSound("music.mp3");
-    song2 = loadSound("music2.mp3");
+    song = loadSound("music1.mp3");
+    song2 = loadSound("music.mp3");
     
     }
 
@@ -25,6 +26,20 @@ function preload(){
 
         function draw(){
             image(video,0,0,600,500);
+            
+song_status=song.isPlaying();
+console.log(song_status);
+console.log(score);
+if(score>0.2){
+    circle(leftwristX,leftwristY,30);
+fill("red");
+song2.stop();
+
+if(song_status == false){
+    song.play();
+    document.getElementById("song_name").innerHTML="The Elegant Captain Hook";
+}
+}
         }
 
         function modelLoaded(){
@@ -39,6 +54,7 @@ function preload(){
 leftwristY=results[0].pose.leftWrist.y;
 rightwristX=results[0].pose.rightWrist.x;
 rightwristY=results[0].pose.rightWrist.y;
+score=results[0].pose.keypoints[9].score;
 
 console.log("rightwristX = "+rightwristX+"  rightwristY = "+rightwristY);
 console.log("leftwristX = "+leftwristX+"  leftwristY = "+leftwristY);
